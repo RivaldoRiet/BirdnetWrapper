01b4f854e70dae28dbed2ed4ba53c8b118d6e128
Rivaldo <rietcoins@inbox.lt>
2021-04-14 12:58:30 +0200

    working string return

--- BirdnetWrapper/Main.cpp
+++ BirdnetWrapper/Main.cpp
@@ -1,80 +1,22 @@
 #include <iostream>
-#ifdef _DEBUG
-#define _DEBUG_WAS_DEFINED
-#undef _DEBUG
-#endif
+#include <string>
+#include <boost/python.hpp>
 
-#include "Python.h"
+namespace py = boost::python;
 
-#ifdef _DEBUG_WAS_DEFINED
-#define _DEBUG
-#undef _DEBUG_WAS_DEFINED
-#endif
+int main(int argc, char* argv[]) {
+	// Initialize the Python interpreter.
+	Py_Initialize();
 
-int main(int argc, char* argv[])
-{
-    PyObject* pName, * pModule, * pFunc;
-    PyObject* pArgs, * pValue;
-    int i;
+	// Add the current directory to the path.
+	py::import("sys").attr("path").attr("append")(".");
 
-    std::string pythonScriptName = "mytest";
-    std::string pythonFunctionName = "multiply";
+	// Get the "hello_from_python" function from "hello.py".
+	py::object hello_from_python = py::import("mytest").attr("hello_from_python");
 
-    Py_Initialize();
-    pName = PyUnicode_DecodeFSDefault(pythonScriptName.c_str());
-    /* Error checking of pName left out */
+	// Call the "hello_from_python" and extract the return value as a string.
+	std::string return_value = py::extract<std::string>(hello_from_python());
 
-    pModule = PyImport_Import(pName);
-    Py_DECREF(pName);
-
-    if (pModule != NULL) {
-        pFunc = PyObject_GetAttrString(pModule, pythonFunctionName.c_str());
-        /* pFunc is a new reference */
-
-        if (pFunc && PyCallable_Check(pFunc)) {
-            pArgs = PyTuple_New(2);
-            for (i = 0; i < 2; ++i) {
-                pValue = PyLong_FromLong(2);
-                if (!pValue) {
-                    Py_DECREF(pArgs);
-                    Py_DECREF(pModule);
-                    fprintf(stderr, "Cannot convert argument\n");
-                    return 1;
-                }
-                /* pValue reference stolen here: */
-                PyTuple_SetItem(pArgs, i, pValue);
-            }
-            pValue = PyObject_CallObject(pFunc, pArgs);
-            Py_DECREF(pArgs);
-            if (pValue != NULL) {
-                printf("Result of call: %ld\n", PyLong_AsLong(pValue));
-                Py_DECREF(pValue);
-            }
-            else {
-                Py_DECREF(pFunc);
-                Py_DECREF(pModule);
-                PyErr_Print();
-                fprintf(stderr, "Call failed\n");
-                return 1;
-            }
-        }
-        else {
-            if (PyErr_Occurred())
-                PyErr_Print();
-            fprintf(stderr, "Cannot find function \"%s\"\n", pythonScriptName.c_str());
-        }
-        Py_XDECREF(pFunc);
-        Py_DECREF(pModule);
-    }
-    else {
-        PyErr_Print();
-        fprintf(stderr, "Failed to load \"%s\"\n", pythonFunctionName.c_str());
-        return 1;
-    }
-    if (Py_FinalizeEx() < 0) {
-        return 120;
-    }
-
-    std::getchar();
-    return 0;
+	// Print out the return value.
+	std::cout << "Return value: '" << return_value << "'" << std::endl;
 }
--- BirdnetWrapper/mytest.py
+++ BirdnetWrapper/mytest.py
@@ -1,17 +1,2 @@
-import math
-
-def myabs(x):
-    return math.fabs(x)
-
-x = 1
-if x == 1:
-    # indented four spaces
-    print("x was 1.")
-
-
-def multiply(a, b):
-    print ('Will compute', a, 'times', b)
-    c = 7
-    for i in range(0, a):
-        c = c + b
-    return c
+def hello_from_python ():
+  return 'Hello from Python!'
