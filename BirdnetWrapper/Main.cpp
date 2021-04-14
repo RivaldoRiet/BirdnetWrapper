78c27e498a28691c7e0295f426ce3a0dbce69d83
Rivaldo <rietcoins@inbox.lt>
2021-04-14 20:56:38 +0200

    hier gebleven

--- BirdnetWrapper/Main.cpp
+++ BirdnetWrapper/Main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 #include <string>
 #include <boost/python.hpp>
+#include <boost/python/numpy.hpp>
+#include <vector>
 
 namespace py = boost::python;
+namespace np = boost::python::numpy;
+
+template<typename T>
+inline
+std::vector< T > py_list_to_std_vector(const boost::python::object& iterable)
+{
+	return std::vector< T >(boost::python::stl_input_iterator< T >(iterable),
+		boost::python::stl_input_iterator< T >());
+}
 
 int main(int argc, char* argv[]) {
 	// Initialize the Python interpreter.
@@ -13,10 +24,20 @@ int main(int argc, char* argv[]) {
 
 	// Get the "hello_from_python" function from "hello.py".
 	py::object hello_from_python = py::import("mytest").attr("hello_from_python");
+	py::object array_from_python = py::import("mytest").attr("writeResultsToArray");
 
 	// Call the "hello_from_python" and extract the return value as a string.
-	std::string return_value = py::extract<std::string>(hello_from_python());
+	//std::vector return_value = py::extract<std::string>(array_from_python());
+	std::string return_value1 = py::extract<std::string>(hello_from_python());
 
-	// Print out the return value.
-	std::cout << "Return value: '" << return_value << "'" << std::endl;
+	// Print out the return value.//
+	//std::cout << "Return value: '" << return_value << "'" << std::endl;
+}
+
+template< typename T >
+inline
+std::vector< T > to_std_vector(const py::object& iterable)
+{
+	return std::vector< T >(py::stl_input_iterator< T >(iterable),
+		py::stl_input_iterator< T >());
 }
--- BirdnetWrapper/mytest.py
+++ BirdnetWrapper/mytest.py
@@ -1,2 +1,35 @@
 def hello_from_python ():
   return 'Hello from Python!'
+
+
+def writeResultsToArray():
+    my_list = list()
+    rcnt = 0
+    for x in range(0, 15):
+        my_list.append('AAAAAAAAAAAAAAAAAAAAAAAA')
+        rcnt += 1
+    print('DONE! WROTE', rcnt, 'RESULTS.')
+    return my_list
+
+get_dir_list.get_dir_list(os.listdir('.'))
+print(writeResultsToArray())
+
+
+""" Birdnet file code
+def writeResultsToFile(detections, min_conf, path):
+
+    print('WRITING RESULTS TO', path, '...', end=' ')
+    rcnt = 0
+    with open(path, 'w') as rfile:
+        rfile.write('Start (s);End (s);Scientific name;Common name;Confidence\n')
+        for d in detections:
+            for entry in detections[d]:
+                if entry[1] >= min_conf and (entry[0] in WHITE_LIST or len(WHITE_LIST) == 0):
+                    rfile.write(d + ';' + entry[0].replace('_', ';') + ';' + str(entry[1]) + '\n')
+                    rcnt += 1
+    print('DONE! WROTE', rcnt, 'RESULTS.')
+
+
+      for d in detections: << this array contains 
+
+"""
