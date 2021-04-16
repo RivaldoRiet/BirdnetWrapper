982e6e3181d4edb69e9fbf585b21c8c64788ec2b
Rivaldo <rietcoins@inbox.lt>
2021-04-16 11:34:28 +0200

    working birdnet connection!

--- BirdnetWrapper/Main.cpp
+++ BirdnetWrapper/Main.cpp
@@ -7,22 +7,38 @@
 namespace py = boost::python;
 namespace np = boost::python::numpy;
 
-template<typename T>
-inline
-std::vector< T > py_list_to_std_vector(const boost::python::object& iterable)
+// decode a Python exception into a string
+std::string handle_pyerror()
 {
-	return std::vector< T >(boost::python::stl_input_iterator< T >(iterable),
-		boost::python::stl_input_iterator< T >());
+	using namespace boost::python;
+	using namespace boost;
+
+	PyObject* exc, * val, * tb;
+	object formatted_list, formatted;
+	PyErr_Fetch(&exc, &val, &tb);
+	handle<> hexc(exc), hval(allow_null(val)), htb(allow_null(tb));
+	object traceback(import("traceback"));
+	if (!tb) {
+		object format_exception_only(traceback.attr("format_exception_only"));
+		formatted_list = format_exception_only(hexc, hval);
+	}
+	else {
+		object format_exception(traceback.attr("format_exception"));
+		formatted_list = format_exception(hexc, hval, htb);
+	}
+	formatted = str("\n").join(formatted_list);
+	return extract<std::string>(formatted);
 }
+
 
 int main(int argc, char* argv[]) {
 	// Initialize the Python interpreter.
 	Py_Initialize();
 
 	// Add the current dir//r("path").attr("append")(".");
-
-	py::object array_from_python = py::import("analyze").attr("writeResultsToArray");
-	//py::object array_from_python = py::import("analyze").attr("loadModel");
+	try {
+	//py::object array_from_python = py::import("analyze").attr("writeResultsToArray");
+	py::object array_from_python = py::import("analyze").attr("loadModel");
 
 	py::list abc = py::extract<py::list>((array_from_python()));
 	py::ssize_t n = py::len(abc);
@@ -31,30 +47,21 @@ int main(int argc, char* argv[]) {
 		std::string up = py::extract<std::string>(elem);
 		std::cout << "Elem value: '" << up << "'" << std::endl;
 	}
+std::cout << "Return value: '" << len(abc) << "'" << std::endl;
+	}
+	
+	catch (const py::error_already_set&) {
+		if (PyErr_Occurred()) {
+			std::string msg = handle_pyerror();
+			std::cout << "Error value: '" << msg << "'" << std::endl;
+		}
+
+		py::handle_exception();
+		PyErr_Clear();
+	}
 
 	//std::string return_value1 = py::extract<std::string>(hello_from_python());
 
 	// Print out the return value.//
-	std::cout << "Return value: '" << len(abc) << "'" << std::endl;
-}
-
-boost::python::list extract_list(py::object x)
-{
-	py::extract<py::list> get_list((x));
-
-	// Make sure we always have the right idea about whether it's a list
-	bool is_list_1 = get_list.check();
-	bool is_list_2 = PyObject_IsInstance(x.ptr(), (PyObject*)&PyList_Type);
-	if (is_list_1 != is_list_2) {
-		throw std::runtime_error("is_list_1 == is_list_2 failure.");
-	}
-	return get_list();
-}
-
-template< typename T >
-inline
-std::vector< T > to_std_vector(const py::object& iterable)
-{
-	return std::vector< T >(py::stl_input_iterator< T >(iterable),
-		py::stl_input_iterator< T >());
+	
 }
